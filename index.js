04b09dab6c74b6b82fbdd508ee2994c549f987fd
jsingle345 <jsingle345@gmail.com>
2020-11-01 17:44:37 -0600

    commit tree 2e09891c2908a14de6c580bd57d79c2e5b834ad9

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-function fetchBooks() {
+const fetchBooks = () => {
   return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks(json))
+  .then(json => RenderBooks(json))
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fetchBooks = () => {
+//   return fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(json => renderBooks(json))
+// }
+
+// const renderBooks = (books) => {
+//   const main = document.querySelector('main')
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = book.name
+//     main.appendChild(h2)
+//   })
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// })
+
